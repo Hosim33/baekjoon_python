fc35ca8453fe1cc777e577801080707d28c577f2
hosim33 <ghtla0330@naver.com>
2024-02-04 23:50:36 +0900

    240204 3040

---
+++ 3040.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+fake1=0
+fake2=0
+
+kid=[int(input()) for _ in range(9)]
+sum=sum(kid)
+
+for j in range(8):
+  for k in range(j+1, 9):
+    if sum - (kid[j] + kid[k]) == 100:
+      fake1 = kid[j]
+      fake2 = kid[k]
+      break
+
+kid.remove(fake1)
+kid.remove(fake2)
+for j in kid:
+    print(j)
